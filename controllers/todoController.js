4921343a27a3143c1bed1b4b20678fe39a261652
mihsh5 <github123@india.com>
2018-07-02 00:29:51 +0530

    Adds the model and controller for ToDos

---
+++ controllers/todoController.js
@@ -0,0 +1,74 @@
+// Library imports
+const ObjectId = require('mongoose').Types.ObjectId;
+
+// Model imports
+const { ToDosModel } = require('../models/todosModel');
+
+function addToDo(toDoToAdd, response) {
+  const newToDo = new ToDosModel(toDoToAdd);
+  newToDo.save((error, result) => {
+    if (error) {
+      console.log(`Couldn't add the ToDo because of the error: ${error}`);
+      response.status(500).send({response: "Couldn't add the ToDo"});
+    } else {
+      response.status(200).send({
+        response: "Added",
+        newToDo: result,
+      });
+    }
+  });
+}
+
+function retrieveToDos(userDetails, response) {
+  ToDosModel.find(userDetails, (error, result) => {
+    if (error) {
+      console.log(`Couldn't fetch the ToDos because of the error: ${error}`);
+      response.status(500).send({response: "Couldn't fetch the ToDos"});
+    } else {
+      response.status(200).send({response: result});
+    }
+  });
+}
+
+function changeStatusOfToDo(todoDetails, response) {
+  const toDoToFindAndUpdate = {
+    _id: new ObjectId(todoDetails._id),
+  };
+  const updates = {
+    $set: {
+      done: todoDetails.done,
+    },
+  };
+  ToDosModel.findOneAndUpdate(toDoToFindAndUpdate, updates, {new: true}, (error, result) => {
+    console.log(result);
+    response.status(200).send({
+      response: "Updated",
+      updatedToDo: result,
+    });
+  });
+}
+
+function deleteAToDo(toDoToDelete, response) {
+  const toDoToFindAndDelete = {
+    _id: new ObjectId(toDoToDelete._id),
+  };
+  ToDosModel.findOneAndRemove(toDoToFindAndDelete, (error, result) => {
+    if(error) {
+      console.log(`Couldn't delete the ToDo because of the error: ${error}`);
+      response.status(500).send({
+        response: "Couldn't delete the ToDo",
+      });
+    } else {
+      response.status(200).send({
+        response: "Deleted",
+      })
+    }
+  });
+}
+
+module.exports = {
+  addToDo,
+  changeStatusOfToDo,
+  deleteAToDo,
+  retrieveToDos,
+};
--- index.js
+++ index.js
@@ -1,23 +1,26 @@
+// Libraries
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors')
-
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const multer = require('multer');
-// const MongoClient = require('mongodb').MongoClient;
 
+// Controllers
 const {addUser, logIn} = require('./controllers/userController');
-const {addToDo, changeStatusOfToDo, deleteToDo, retrieveToDos} = require('./todoController');
+const {addToDo, retrieveToDos, changeStatusOfToDo, deleteAToDo} = require('./controllers/todoController');
+
+// Constants
 const {CONNECTION_URI, DB_NAME} = require('./constants');
 
 const upload = multer();
 
 const app = express();
+
 const corsOptions = {
   origin: ['https://1vz2zrvpq4.codesandbox.io', 'https://v3963rqvly.codesandbox.io']
 };
-app.use(cors(corsOptions));
 
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -34,54 +37,22 @@ app.post('/login', upload.array(), function(request, response) {
 });
 
 app.post('/newToDo', upload.array(), function(request, response) {
-  MongoClient.connect(CONNECTION_URI, function (error, dbClient) {
-    if(error) {
-      console.log('Cannot connect to the database');
-      response.status(500).send({response: "Cannot connect to the database"});
-      return;
-    }
-    const dbInstance = dbClient.db(DB_NAME);
-    addToDo(dbInstance, request.body, response, () => dbClient.close());
-  });
+  addToDo(request.body, response);
 });
 
 app.post('/changeStatusOfToDo', upload.array(), function(request, response) {
-  MongoClient.connect(CONNECTION_URI, function(error, dbClient) {
-    if(error) {
-      console.log('Cannot connect to the database');
-      response.status(500).send({response: "Cannot connect to the database"});
-      return;
-    }
-    const dbInstance = dbClient.db(DB_NAME);
-    changeStatusOfToDo(dbInstance, request.body, response, () => dbClient.close());
-  });
+  changeStatusOfToDo(request.body, response);
 });
 
 app.delete('/deleteToDo', upload.array(), function(request, response) {
-  MongoClient.connect(CONNECTION_URI, function(error, dbClient) {
-    if(error) {
-      console.log('Cannot connect to the database');
-      response.status(500).send({response: "Cannot connect to the database"});
-      return;
-    }
-    const dbInstance = dbClient.db(DB_NAME);
-    deleteToDo(dbInstance, request.body, response, () => dbClient.close());
-  });
+  deleteAToDo(request.body, response);
 });
 
 app.post('/getToDos', upload.array(), function(request, response) {
-  MongoClient.connect(CONNECTION_URI, function(error, dbClient) {
-    if(error) {
-      console.log('Cannot connect to the database');
-      response.status(500).send({response: "Cannot connect to the database"});
-      return;
-    }
-    const dbInstance = dbClient.db(DB_NAME);
-    retrieveToDos(dbInstance, request.body.userId, response, () => dbClient.close());
-  });
+  retrieveToDos(request.body, response);
 });
 
 app.get('/about', function(request, response) {
-  response.send('This is Mihir');
+  response.send("This is Mihir Shah's ToDo App");
 });
 app.listen(3000);
